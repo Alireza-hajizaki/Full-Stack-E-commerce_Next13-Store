2cd87781489f5328aa5c25b0f079858b52b6e002
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-08-30 18:04:13 +0330

    creted ui & billboard & categories & actions

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,19 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0 ; 
+
+const HomePage = async () => {
+
+    const billboard = await getBillboard("8b752ddf-a359-484f-8419-8d82a9d3b796");
+
     return ( 
-        <div>Home Page</div>
+       <Container>
+        <div className="space-y-10 pb-10">  
+          <Billboard data={billboard} />
+        </div>
+       </Container>
      );
 }
  
