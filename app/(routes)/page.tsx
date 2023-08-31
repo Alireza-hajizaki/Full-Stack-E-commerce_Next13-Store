5994131925e95eaed5c7595fc2708296e225abdf
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-08-31 16:32:01 +0330

    products card screen copmleted

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,23 +4,22 @@ import Billboard from "@/components/billboard";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
-export const revalidate = 0 ; 
+export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("8b752ddf-a359-484f-8419-8d82a9d3b796");
 
-    const products = await getProducts({isFeatured: true});
-    const billboard = await getBillboard("8b752ddf-a359-484f-8419-8d82a9d3b796");
-
-    return ( 
-       <Container>
-        <div className="space-y-10 pb-10">  
-          <Billboard data={billboard} />
-        </div>
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title='Featured Products' items={products} />
+          <ProductList title="Featured Products" items={products} />
         </div>
-       </Container>
-     );
-}
- 
+      </div>
+    </Container>
+  );
+};
+
 export default HomePage;
